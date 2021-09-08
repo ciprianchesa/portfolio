d57068ba239e5d049dc947475d9a8c1c98f47396
Ciprian Chesa <chesa_ciprian@yahoo.com>
2021-09-08 10:07:16 +0300

    add js on nav, mobile nav, page display with js

--- functions.js
+++ functions.js
@@ -1,3 +1,45 @@
+function mobileNavbar() {
+    var x = document.getElementById("navbar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+function hide(id) {
+  const el = document.getElementById(id)
+  el.style.display = 'none';
+}
+
+function show(id) {
+    document.getElementById(id).style.display = '';
+}
+
+function hideAllPages() {
+    const pages = Array.from(document.querySelectorAll(".page"));
+    pages.forEach(page => hide(page.id));
+}
+
+function showPage(pageId) {
+    hideAllPages();
+    show(pageId);
+}
+
+function listenMenuClicks() {
+  document.addEventListener("click", e => {
+  const link = e.target;
+  if (link.matches("#top-menu-bar a")) {  
+      const id = link.getAttribute("data-id");
+      showPage(id);
+      }
+  });
+};
+
+listenMenuClicks();
+
+showPage("home");
+
 (function() {
     emailjs.init("user_SHLKn2xBkl3ckoRqEHfkt");
     })();
